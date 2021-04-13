f17fcb7103d926054038e7188cb889ff5abba1f9
melodeenae5 <danaekirby5@gmail.com>
2021-04-13 15:46:35 -0500

    completed user register and login routes

--- routes/users.js
+++ routes/users.js
@@ -7,10 +7,62 @@ const utils = require('../lib/utils');
 // TODO
 router.get('/protected', (req, res, next) => {});
 
-// TODO
-router.post('/login', function (req, res, next) {});
+router.post('/login', function (req, res, next) {
+	User.findOne({ username: req.body.username })
+		.then((user) => {
+			if (!user) {
+				res.status(401).json({ success: false, msg: 'could not find user' });
+			}
+			const isValid = utils.validPassword(
+				req.body.password,
+				user.hash,
+				user.salt
+			);
 
-// TODO
-router.post('/register', function (req, res, next) {});
+			if (isValid) {
+				const tokenObject = utils.issueJWT(user);
+
+				res.status(200).json({
+					success: true,
+					token: tokenObject.token,
+					expiresIn: tokenObject.expires,
+				});
+			} else {
+				res
+					.status(401)
+					.json({ success: false, msg: 'you entered the wrong password' });
+			}
+		})
+		.catch((err) => {
+			next(err);
+		});
+});
+
+router.post('/register', function (req, res, next) {
+	const saltHash = utils.genPassword(req.body.password);
+
+	const salt = saltHash.salt;
+	const hash = saltHash.hash;
+
+	const newUser = new User({
+		username: req.body.username,
+		hash: hash,
+		salt: salt,
+	});
+	try {
+		newUser.save().then((user) => {
+			const jwt = utils.issueJWT(user);
+
+			res.json({
+				success: true,
+				user: user,
+				token: jwt.token,
+				expiresIn: jwt.expires,
+			});
+		});
+	} catch (err) {
+		res.json({ success: false, msg: err });
+	}
+});
 
 module.exports = router;
