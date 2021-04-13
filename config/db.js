f3ed11cc0b4b9b7f7742ffa62f7f9f62b9250e2c
melodeenae5 <danaekirby5@gmail.com>
2021-04-13 11:00:46 -0500

    fixed database issue

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 const config = require('config');
 const db = config.get('mongoURI');
 
---
+++ config/default.js
@@ -0,0 +1,3 @@
+module.exports = {
+	mongoURI: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vdyxm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+};
