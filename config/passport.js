6475d93bb7adfdba2fa161b8269c5eb5c7430836
melodeenae5 <danaekirby5@gmail.com>
2021-04-13 15:18:17 -0500

    added jwt verify callback

--- config/passport.js
+++ config/passport.js
@@ -7,12 +7,24 @@ const ExtractJwt = require('passport-jwt').ExtractJwt;
 const pathToKey = path.join(__dirname, '..', 'id_rsa_pub.pem');
 const PUB_KEY = fs.readFileSync(pathToKey, 'utf8');
 
-// TODO
 const options = {
 	jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
 	secretOrKey: PUB_KEY,
 	algorithms: ['RS256'],
 };
 
-// TODO
-module.exports = (passport) => {};
+const strategy = new JwtStrategy(options, (payload, done) => {
+	User.findOne({ _id: payload.sub })
+		.then((user) => {
+			if (user) {
+				return done(null, user);
+			} else {
+				return done(null, false);
+			}
+		})
+		.catch((err) => done(err, null));
+});
+
+module.exports = (passport) => {
+	passport.use(strategy);
+};
