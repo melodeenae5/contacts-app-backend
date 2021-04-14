f9e33bb95a0f4d321a7529c634f63a710f70b07d
melodeenae5 <danaekirby5@gmail.com>
2021-04-13 19:37:30 -0500

    removed unnnecessary comments

--- config/db.js
+++ config/db.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const config = require('config');
 const db = config.get('mongoURI');
-// const UserSchema = require('../models/User');
 
 mongoose.connect(db, {
 	useNewUrlParser: true,
@@ -12,33 +11,3 @@ mongoose.connect(db, {
 mongoose.connection.on('connected', () => {
 	console.log('Database connected');
 });
-// const connection = mongoose.createConnection(db, {
-// 	useNewUrlParser: true,
-// 	useUnifiedTopology: true,
-// });
-
-// const UserSchema = new mongoose.Schema({
-// 	username: {
-// 		type: String,
-// 		required: true,
-// 	},
-// 	hash: String,
-// 	salt: String,
-// });
-
-// const User = connection.model('User', UserSchema);
-
-// const connectDB = async () => {
-// 	try {
-// 		await mongoose.connect(db, {
-// 			useNewUrlParser: true,
-// 		});
-
-// 		console.log('MongoDB is Connected...');
-// 	} catch (err) {
-// 		console.error(err.message);
-// 		process.exit(1);
-// 	}
-// };
-
-// module.exports = connection;
